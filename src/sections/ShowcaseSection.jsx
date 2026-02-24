49934d5e98fd04705b98a942bd882ff866d16f1f
anil-titung-tamang <anil.titung.tamang@gmail.com>
2026-02-24 21:55:25 +0545

    initial commit

---
+++ src/sections/ShowcaseSection.jsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import { useRef } from 'react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(ScrollTrigger)
+
+
+const ShowcaseSection = () => {
+    const sectionRef = useRef(null)
+    const project1Ref = useRef(null)
+    const project2Ref = useRef(null)
+    const project3Ref = useRef(null)
+ 
+
+    useGSAP(() => {
+           const projects = [project1Ref.current, project2Ref.current,
+            project3Ref.current]
+        
+        
+      projects.forEach((card, index) => {
+        gsap.fromTo(
+            card,
+            {
+                y: 50, opacity: 0
+            },
+            {
+                y: 0, 
+                opacity: 1,
+                duration: 1,
+                delay: 0.3 * (index + 1),
+                scrollTrigger: {
+                    trigger: card,
+                    start: 'top bottom-=100',
+                }
+            }
+        )
+    })  
+        gsap.fromTo(
+        sectionRef.current,
+        { opacity: 0 },
+        { opacity: 1, duaration: 1.5 })  
+    },[])
+
+
+
+
+  return (
+      <section id="work" ref={sectionRef} className="app-showcase">
+          <div className="w-full">
+              <div className="showcaselayout">
+                  {/*left */}
+                  <div className="first-project-wrapper" ref={project1Ref}>
+                      <div className="image-wrapper">
+                          <img src="/images/project1.png" alt="dhukuti"/>
+                          
+                      </div>
+
+                      <div className="text-content">
+                          <h2>Shared Digital-Treasure Anytime-Anywhere</h2>
+
+                          <p className="text-white-50 md:text-xl">
+                              
+                              An Web-Application buit using Django, Python & TailwindCSS 
+                              for a fast user-friendly experience.
+                              
+                          </p>
+                          
+                      </div>
+                      
+                  </div>
+
+
+
+                  {/*right */}
+
+                  <div className="project-list-wrapper overflow-hidden">
+                      <div className="project" ref={project2Ref}>
+                          
+                          <div className="image-wrapper bg-[#ffefdb]">
+                              <img src="/images/project2.png" alt="Medimate"/>
+                          </div>
+
+                          <h2>
+                              Ai-Powered Health Assistant
+                          </h2>
+                          
+                      </div>
+
+                      <div className="project" ref={project3Ref}>
+                          
+                          <div className="image-wrapper bg-[#ffe7eb]">
+                              <img src="/images/project3.png" alt="MeriAama"/>
+                          </div>
+
+                          <h2>
+                              Ai-Powered Pregnancy Health Assistant
+                          </h2>
+                          
+                      </div>
+                      
+                  </div>
+
+
+                  
+              </div>              
+          </div>      
+    </section>
+  )
+}
+
+export default ShowcaseSection
