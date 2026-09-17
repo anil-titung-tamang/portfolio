3e9756da1789abe9dd9c963a5e3bd6e14aed1bb0
anil-titung-tamang <anil.titung.tamang@gmail.com>
2026-09-17 12:36:47 +0545

    update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
   return (
       <section id="hero" className="relative overflow-hidden">
           <div className="absolute top-0 left-0 z-10">
-              <img src="public\images\bg.png" alt="background" />
+              <img src="/images/bg.png" alt="background" />
           </div>
 
 
